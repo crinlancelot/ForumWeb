2347c971f38e2f7c26c8b9fe1bd0348d2310d569
Carlos Felipe Escobar <crinlancelot@hotmail.com>
2018-03-10 12:35:39 -0600

    Cleanup server project

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  res.json({
+  	data: "hello world",
+  });
 });
+
+  router.get('/about', (req, res) => {
+
+  	res.json({
+  		name: 'forum-app',
+  		version: '0.1.0',
+  	})
+  });
 
 module.exports = router;
